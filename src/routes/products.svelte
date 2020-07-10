9e11decab86a742f27eb338fd0d12bad1b247e60
Jonathan Hudak <jmhudak@amazon.com>
2020-07-09 22:51:11 -0700

    Snipcart 2.0 for subscriptions

--- src/routes/products.svelte
+++ src/routes/products.svelte
@@ -9,18 +9,24 @@
 
 <svelte:head>
   <title>Products</title>
-  <link
-    rel="stylesheet"
-    href="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.css" />
-  <script src="https://cdn.snipcart.com/themes/v3.0.17/default/snipcart.js">
+
+  <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous">
 
   </script>
-</svelte:head>
+  <script
+    src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
+    data-api-key="NTcyNGUzNmEtODgzNC00YjExLTgzZWQtM2E1Y2M4NWM2NDYwNjM3Mjk5NTEzNjAzMTMyMjA0"
+    id="snipcart">
 
-<div
-  id="snipcart"
-  data-api-key="NTcyNGUzNmEtODgzNC00YjExLTgzZWQtM2E1Y2M4NWM2NDYwNjM3Mjk5NTEzNjAzMTMyMjA0"
-  hidden />
+  </script>
+  <link
+    href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css"
+    rel="stylesheet"
+    type="text/css" />
+</svelte:head>
 
 <Hero />
 <Buy />
